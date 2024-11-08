7fd9b12730e60eaad0d0708e810415683665103e
yassyolo <yoana.yotova03@gmail.com>
2024-11-08 12:05:28 +0200

    Added service for uploading files to google drive and obtaining their url

---
+++ LogiTrack.Core/Services/GoogleDriveService.cs
@@ -0,0 +1,77 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Drive.v3;
+using Google.Apis.Services;
+using LogiTrack.Core.Contracts;
+
+namespace LogiTrack.Core.Services
+{
+    public class GoogleDriveService : IGoogleDriveService
+    {
+        private readonly string serviceAccountJson = @"{
+ test
+}";
+
+
+        public DriveService GetDriveServiceAsync()
+        {
+            GoogleCredential credentials = GoogleCredential.FromJson(serviceAccountJson)
+                                                           .CreateScoped(DriveService.Scope.DriveFile);
+
+            return new DriveService(new BaseClientService.Initializer
+            {
+                HttpClientInitializer = credentials,
+                ApplicationName = "LogiTrack"
+            });
+        }
+
+        public async Task<string> UploadFileAsync(string filePath, string mimeType, string? folderId = null)
+        {
+            var driveService = GetDriveServiceAsync();
+
+            var fileMetadata = new Google.Apis.Drive.v3.Data.File
+            {
+                Name = Path.GetFileName(filePath),
+                MimeType = mimeType,
+                Parents = folderId != null ? new List<string> { folderId } : null
+            };
+
+            using (var fileStream = new FileStream(filePath, FileMode.Open))
+            {
+                var request = driveService.Files.Create(fileMetadata, fileStream, mimeType);
+                request.Fields = "id";
+
+                var uploadProgress = await request.UploadAsync();
+
+                if (uploadProgress.Status == Google.Apis.Upload.UploadStatus.Completed)
+                {
+                    return request.ResponseBody.Id;
+                }
+                else
+                {
+                    throw new Exception($"Error while uploading file: {uploadProgress.Exception}");
+                }
+            }
+        }
+
+        public async Task<IList<Google.Apis.Drive.v3.Data.File>> ListFilesAsync(int pageSize = 10)
+        {
+            var driveService = GetDriveServiceAsync();
+            var request = driveService.Files.List();
+            request.PageSize = pageSize;
+            request.Fields = "nextPageToken, files(id, name)";
+
+            var result = await request.ExecuteAsync();
+            return result.Files;
+        }
+
+        public async Task<string> GetFileUrlAsync(string fileId)
+        {
+            var driveService = GetDriveServiceAsync();    
+            var request = driveService.Files.Get(fileId);
+            request.Fields = "webViewLink, webContentLink";
+
+            var file = await request.ExecuteAsync();
+            return file.WebViewLink ?? file.WebContentLink;
+        }
+    }
+}
