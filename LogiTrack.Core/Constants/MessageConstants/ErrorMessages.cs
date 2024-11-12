4df3c195bb78946d9d449adcb401b39a8a900dba
yassyolo <yoana.yotova03@gmail.com>
2024-11-13 01:49:37 +0200

    Added service for handling logic for clients

--- LogiTrack.Core/Constants/MessageConstants/ErrorMessages.cs
+++ LogiTrack.Core/Constants/MessageConstants/ErrorMessages.cs
@@ -5,6 +5,13 @@
         public const string DeliveryNotFoundErrorMessage = "Delivery not found.";
         public const string DriverDoesNotHaveDeliveryErrorMessage = "Driver does not have this delivery.";
         public const string DriverNotFoundErrorMessage = "Driver not found.";
+        public const string InvalidAddressErrorMessage = "Address is invalid.";
+        public const string ClientCompanyNotFoundErrorMessage = "Client company not found.";
+        public const string RequestNotFoundErrorMessage = "Request not found.";
+        public const string CompanyDoesNotHaveRequestErrorMessage = "Company does not have this request.";
+        public const string OfferNotFoundErrorMessage = "Offer not found.";
+        public const string CompanyDoesNotHaveOfferErrorMessage = "Company does not have this offer.";
+        public const string CompanyDoesNotHaveDeliveryErrorMessage = "Company does not have this delivery.";
         public const string InvalidCoordinatesErrorMessage = "Invalid coordinates.";
         public const string LicenseExpirationErrorMessage = "Your driver's license will expire soon.";
         public const string RequiredFieldErrorMessage = "{0} is required.";
@@ -12,6 +19,13 @@
         public const string InvoiceNotFoundErrorMessage = "Invoice not found.";
         public const string InvalidPriceErrorMessage = "Invalid price.";
         public const string VehicleNotFoundErrorMessage = "Vehicle not found.";
-
+        public const string InvalidLoginAttemptErrorMessage = "Invalid login attempt.";
+        public const string InvalidEmailErrorMessage = "Email address is invalid.";
+        public const string InvalidAmountErrorMessage = "Invalid amount.";
+        public const string EmailAlreadyExistsErrorMessage = "User with this email already exists.";
+        public const string PhoneNumberAlreadyExistsErrorMessage = "User with this phone number already exists.";
+        public const string UserNotFoundErrorMessage = "User not found.";
+        public const string PasswordsDoNotMatchErrorMessage = "Passwords do not match.";
+        public const string NewPasswordLikeCurrentPasswordErrorMessage = "New password cannot be the same as the current password.";
     }
 }
---
+++ LogiTrack.Core/CustomExceptions/ClientCompanyNotFoundException.cs
@@ -0,0 +1,10 @@
+﻿using static LogiTrack.Core.Constants.MessageConstants.ErrorMessages;
+namespace LogiTrack.Core.CustomExceptions
+{
+    public class ClientCompanyNotFoundException : Exception
+    {
+        public ClientCompanyNotFoundException() : base(ClientCompanyNotFoundErrorMessage)
+        {
+        }
+    }
+}
