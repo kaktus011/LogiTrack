48c5d1cdefbe699393c844ae2d15b297c64850b2
yassyolo <yoana.yotova03@gmail.com>
2024-11-13 01:47:26 +0200

    Added and changed folders of the viewmodels for better readability

---
+++ LogiTrack.Core/ViewModels/Clients/ContactDetailsViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace LogiTrack.Core.ViewModels.Clients
+{
+    public class ContactDetailsViewModel
+    {
+        public int Id { get; set; }
+        public string Username { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string ContactPerson { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
+        public string AlternativePhoneNumber { get; set; } = string.Empty;
+    }
+}
---
+++ LogiTrack.Core/ViewModels/Clients/PendingRegistrationsViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace LogiTrack.Core.ViewModels.Clients
+{
+    public class PendingRegistrationsViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;       
+        public string RegistrationNumber { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
+        public string CreatedAt { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+    }
+}
