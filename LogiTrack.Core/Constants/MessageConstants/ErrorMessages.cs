83f2c0e3f8e669cad0e42829c08047926adff798
yassyolo <yoana.yotova03@gmail.com>
2024-11-29 20:17:24 +0200

    Added constants for vehicle status and types for cash registers, events and vehicles

--- LogiTrack.Core/Constants/MessageConstants/ErrorMessages.cs
+++ LogiTrack.Core/Constants/MessageConstants/ErrorMessages.cs
@@ -1,31 +1,31 @@
 ﻿namespace LogiTrack.Core.Constants.MessageConstants
 {
-    public class ErrorMessages
+    public static class ErrorMessages
     {
-        public const string DeliveryNotFoundErrorMessage = "Delivery not found.";
-        public const string DriverDoesNotHaveDeliveryErrorMessage = "Driver does not have this delivery.";
-        public const string DriverNotFoundErrorMessage = "Driver not found.";
-        public const string InvalidAddressErrorMessage = "Address is invalid.";
-        public const string ClientCompanyNotFoundErrorMessage = "Client company not found.";
-        public const string RequestNotFoundErrorMessage = "Request not found.";
-        public const string CompanyDoesNotHaveRequestErrorMessage = "Company does not have this request.";
-        public const string OfferNotFoundErrorMessage = "Offer not found.";
-        public const string CompanyDoesNotHaveOfferErrorMessage = "Company does not have this offer.";
-        public const string CompanyDoesNotHaveDeliveryErrorMessage = "Company does not have this delivery.";
-        public const string InvalidCoordinatesErrorMessage = "Invalid coordinates.";
-        public const string LicenseExpirationErrorMessage = "Your driver's license will expire soon.";
         public const string RequiredFieldErrorMessage = "{0} is required.";
-        public const string LengthErrorMessage = "{0} should be between {2} and {1} characters.";
-        public const string InvoiceNotFoundErrorMessage = "Invoice not found.";
-        public const string InvalidPriceErrorMessage = "Invalid price.";
-        public const string VehicleNotFoundErrorMessage = "Vehicle not found.";
-        public const string InvalidLoginAttemptErrorMessage = "Invalid login attempt.";
         public const string InvalidEmailErrorMessage = "Email address is invalid.";
-        public const string InvalidAmountErrorMessage = "Invalid amount.";
+        public const string InvalidAddressErrorMessage = "Address is invalid.";
+        public const string InvalidLoginAttemptErrorMessage = "Invalid login attempt.";
+        public const string LengthErrorMessage = "{0} should be between {2} and {1} characters.";
         public const string EmailAlreadyExistsErrorMessage = "User with this email already exists.";
         public const string PhoneNumberAlreadyExistsErrorMessage = "User with this phone number already exists.";
         public const string UserNotFoundErrorMessage = "User not found.";
-        public const string PasswordsDoNotMatchErrorMessage = "Passwords do not match.";
+        public const string ClientCompanyNotFoundErrorMessage = "Client company not found.";
+        public const string VehicleNotFoundErrorMessage = "Vehicle not found.";
+        public const string InvalidPriceErrorMessage = "Invalid price.";
+        public const string DeliveryNotFoundErrorMessage = "Delivery not found.";
+        public const string InvalidAmountErrorMessage = "Invalid amount.";
+        public const string PasswordsDoNotMatchErrorMessage= "Passwords do not match.";
         public const string NewPasswordLikeCurrentPasswordErrorMessage = "New password cannot be the same as the current password.";
+        public const string OfferNotFoundErrorMessage = "Offer not found.";
+        public const string CompanyDoesNotHaveOfferErrorMessage = "Company does not have this offer.";
+        public const string DriverNotFoundErrorMessage = "Driver not found.";
+        public const string DriverDoesNotHaveDeliveryErrorMessage = "Driver does not have this delivery.";
+        public const string LicenseExpirationErrorMessage = "Your driver's license will expire soon.";
+        public const string InvalidCoordinatesErrorMessage = "Invalid coordinates.";
+        public const string InvoiceNotFoundErrorMessage = "Invoice not found.";
+        public const string RequestNotFoundErrorMessage = "Request not found.";
+        public const string CompanyDoesNotHaveDeliveryErrorMessage = "Company does not have this delivery.";
+        public const string CompanyDoesNotHaveRequestErrorMessage = "Company does not have this request.";
     }
 }
