637ad4be81da633abf989e78c612e95fd1d66611
yassyolo <yoana.yotova03@gmail.com>
2024-11-29 20:18:21 +0200

    added user not found custom exception

---
+++ LogiTrack.Core/CustomExceptions/UserNotFoundException.cs
@@ -0,0 +1,10 @@
+﻿using static LogiTrack.Core.Constants.MessageConstants.ErrorMessages;
+namespace LogiTrack.Core.CustomExceptions
+{
+    public class UserNotFoundException : Exception
+    {
+        public UserNotFoundException() : base(UserNotFoundErrorMessage)
+        {
+        }
+    }
+}
