1512a8dc949a23399fe33603d6063e59d80a0ea8
yassyolo <yoana.yotova03@gmail.com>
2025-01-12 02:06:21 +0200

    Added view models for requests

--- LogiTrack.Core/ViewModels/Request/PendingRequestDetailsViewModel.cs
+++ LogiTrack.Core/ViewModels/Request/PendingRequestDetailsViewModel.cs
@@ -1,19 +1,29 @@
-﻿using LogiTrack.Core.ViewModels.Delivery;
+﻿using LogisticsSystem.Infrastructure.Data.DataModels;
+using LogiTrack.Core.ViewModels.Delivery;
 
 namespace LogiTrack.Core.ViewModels.Request
 {
     public class PendingRequestDetailsViewModel
     {
+        public int Id { get; set; }
+        public bool GivenOffer { get; set; }
+        public string? OfferNumber { get; set; }
+        public string? OfferDate { get; set; }
+        public string? FinalPrice { get; set; }
+        public int? OfferId { get; set; }
+        public bool Reserved { get; set; }
         public string ReferenceNumber { get; set; } = string.Empty;
         public string ClientAddress { get; set; } = string.Empty;
         public string ClientPhone { get; set; } = string.Empty;
         public string ClientEmail { get; set; } = string.Empty;
-        public string SuggestedStartDate { get; set; } = string.Empty;
+        public DateTime SuggestedStartDate { get; set; } 
+        public int NeededVehiclesCount { get; set; }
+        public List<(PossibleVehiclesForDeliveryViewModel Primary, PossibleVehiclesForDeliveryViewModel Seconary, VehiclePairsLegend Legend)> NeededVehiclePairs { get; set; } = new List<(PossibleVehiclesForDeliveryViewModel Primary, PossibleVehiclesForDeliveryViewModel Seconary, VehiclePairsLegend Legend)>();
         public int ClientId { get; set; }
         public string CreatedOn { get; set; } = string.Empty;
         public string ClientCompanyName { get; set; } = string.Empty;
         public string RegistrationNumber { get; set; } = string.Empty;
-        public string CargoType { get; set; } = string.Empty; //standard , not standard
+        public string CargoType { get; set; } = string.Empty;
         public string? TypeOfPallet { get; set; } = string.Empty;
         public string? NumberOfPallets { get; set; }
         public string? PalletLength { get; set; }
@@ -39,8 +49,11 @@ namespace LogiTrack.Core.ViewModels.Request
         public string SpecialInstructions { get; set; } = string.Empty;
         public string IsRefrigerated { get; set; } = string.Empty;
         public string ApproximatePrice { get; set; } = string.Empty;
+        public int RequiredVehicleCount { get; set; }
         public List<PossibleVehiclesForDeliveryViewModel> PossibleVehicles { get; set; } = new List<PossibleVehiclesForDeliveryViewModel>();
         public List<PossibleDriversForDeliveryViewModel> PossibleDrivers { get; set; } = new List<PossibleDriversForDeliveryViewModel>();
         public IEnumerable<NonStandardCargosViewModel> NonStandardCargos { get; set; } = new List<NonStandardCargosViewModel>();
     }
+
+    
 }
